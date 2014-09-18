fa1eb445499a9151e338a3630e5d71db365d8a3b
JKL <jkolodziejczyk612@gmail.com>
2014-09-18 22:22:12 +0200

    XmlWorker - XmlWorker - init simple XML read/write class

---
+++ XmlWorker/Main/XmlWorker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlWorker
+{
+    public static class XmlWorker
+    {
+        public static DataTree Read(string file)
+        {
+            DataTree data = new DataTree();
+            throw new NotImplementedException();
+        }
+        public static bool Write(DataTree data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
