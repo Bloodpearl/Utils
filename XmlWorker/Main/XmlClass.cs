3aba19c2890c1e806e02aaddbecf582d46d539d1
JKL <jkolodziejczyk612@gmail.com>
2014-09-18 22:21:38 +0200

    XmlWorker - XmlClass - init interface

---
+++ XmlWorker/Main/XmlClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlWorker
+{
+    public interface XmlClass
+    {
+        DataTree GetXmlData();
+        void SetXmlData(DataTree data);
+    }
+}
