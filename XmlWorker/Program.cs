b1714abb5a1f18b67b5239dd5a015afaaa8baf06
JKL <jkolodziejczyk612@gmail.com>
2014-09-19 06:22:49 +0200

    XmlWorker - Tests - test procedure

---
+++ XmlWorker/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XmlWorker.Tests;
+
+namespace XmlWorker.Tests
+{
+    public static class Program
+    {
+        static bool catchExceptions = true;
+        enum TestResult
+        {
+            Failed = 0,
+            Success = 1
+        }
+
+        static void Main()
+        {
+            int success = 1;
+            success = RunTests();
+            Console.WriteLine("Tests complete with result: " + ((TestResult)success).ToString());
+            Console.ReadKey();
+        }
+
+        static int RunTests()
+        {
+            TestFunction[] test = {
+                Tests.DataNode.RunTests,
+                Tests.DataLeaf.RunTests,
+                Tests.DataBranch.RunTests,
+                Tests.DataTree.RunTests
+                                  };
+
+            int success = 1;
+            
+            foreach( TestFunction t in test)
+            {
+                success = RunTest(t) == 1 && success == 1 ? 1 : 0;
+                //if(success == 0) break;
+            }
+             
+            return success;
+        }
+
+        public delegate bool TestFunction(out string name);
+        static int RunTest(TestFunction func)
+        {
+            string name = "";
+            string message = null;
+            TestResult tr = TestResult.Success;
+            if (catchExceptions)
+            {
+                try
+                {
+                    if (!func(out name))
+                        tr = TestResult.Failed;
+                }
+                catch (Exception ex)
+                {
+                    tr = TestResult.Failed;
+                    message = ex.Message;
+                }
+            }
+            else
+            {
+                if (!func(out name))
+                    tr = TestResult.Failed;
+            }
+            Console.WriteLine("Test " + name + " end with status: " + tr.ToString());
+            if (message != null)
+                Console.WriteLine("   " + message);
+            return (int)tr;
+        }
+    }
+}
---
+++ XmlWorker/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlWorker.Tests
+{
+    public abstract class Test
+    {
+        public static bool RunTests( string className, out string current, Program.TestFunction[] tests)
+        {
+            bool success = true;
+            string name;
+            foreach(Program.TestFunction func in tests)
+            {
+                success = func(out name);
+                current = className +"."+name;
+                if(!success)
+                    return success;
+            }
+
+            current = className;
+            return success;
+        }
+    }
+}
